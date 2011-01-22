b2bc0467375198a0589734211881a633c7825955
Jean-François Simon <contact@jfsimon.fr>
2011-01-22 10:37:07 +0100

    added Git Fetch command test

---
+++ tests/Scm/Tests/Command/FetchTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Scm\Tests\Command;
+
+use Scm\Filesystem;
+
+use Scm\Tests\ScmTestCase;
+use Scm\Filesystem\Directory;
+use Scm\Command\Git\Fetch;
+
+class FetchTest extends ScmTestCase
+{
+    public function testGitFetch()
+    {
+        $repos = 'git://github.com/jfsimon/scm.git';
+        $dir = new Directory($this->makeTestDirectory());
+        $fetch = new Fetch($dir->getPath());
+        $fetch->setRepository($repos);
+        $fetch->execute();
+        $this->assertFalse($dir->isEmpty());
+        $this->assertEquals($repos, file_get_contents($dir->getPath().'/data/tests/repository'));
+    }
+}
